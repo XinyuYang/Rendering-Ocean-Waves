c7204971f818b54ba4391e9179ce59fce5fe83a7
Haimeng Zhang <hzhang1@macalester.edu>
2018-11-28 19:00:06 -0600

    Reloaload model mesh from blender

--- src/OceanWave.cpp
+++ src/OceanWave.cpp
@@ -22,7 +22,7 @@ OceanWave::OceanWave(int argc, char** argv) : VRApp(argc, argv)
 	_lastTime = 0.0;
     _curFrameTime = 0.0;
     
-    eye_world = glm::vec3(0, 0, 4);
+    eye_world = glm::vec3(0, 100, 200);
 }
 
 OceanWave::~OceanWave()
@@ -112,7 +112,7 @@ void OceanWave::onRenderGraphicsContext(const VRGraphicsState &renderState) {
         
         // load model
         // -----------
-        _modelMesh.reset(new Model("Ocean.obj", 1.0, glm::vec4(1.0)));
+        _modelMesh.reset(new Model("Ocean2.obj", 1.0, glm::vec4(1.0)));
         
         // load ocean surface texture
         // -----------
@@ -120,35 +120,6 @@ void OceanWave::onRenderGraphicsContext(const VRGraphicsState &renderState) {
 //        _bumpMap->setTexParameteri(GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 //        _bumpMap->setTexParameteri(GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-        // Todo: load envrionment texture Map
-        // string texturefiles[] = {"","","",""}
-        // envrionmentMap = Texture::createCubeMapFromFiles(texturefiles, true, 4)
-        //environmentMap->setTexParameteri(GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-        //skyBox.reset( new SkyBox(envroinmentMap));
-        
-        // framebuffer configuration
-        // -------------------------
-//        unsigned int framebuffer;
-//        glGenFramebuffers(1, &framebuffer);
-//        glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
-//        // create a color attachment texture
-//        unsigned int textureColorbuffer;
-//        glGenTextures(1, &textureColorbuffer);
-//        glBindTexture(GL_TEXTURE_2D, textureColorbuffer);
-//        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
-//        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-//        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-//        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureColorbuffer, 0);
-        // create a renderbuffer object for depth and stencil attachment (we won't be sampling these)
-//        unsigned int rbo;
-//        glGenRenderbuffers(1, &rbo);
-//        glBindRenderbuffer(GL_RENDERBUFFER, rbo);
-//        glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, SCR_WIDTH, SCR_HEIGHT); // use a single renderbuffer object for both a depth AND stencil buffer.
-//        glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo); // now actually attach it
-//        // now that we actually created the framebuffer and added all attachments we want to check if it is actually complete now
-//        if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
-//            cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!" << endl;
-//        glBindFramebuffer(GL_FRAMEBUFFER, 0);
         
     }
 }
